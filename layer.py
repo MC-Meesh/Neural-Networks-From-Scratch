21507471cce39306dd2a6c86c245f8b644105170
Michael Allen <chaseallenskis@gmail.com>
2023-10-14 10:12:50 -0600

    Annotated abstract methods

--- layer.py
+++ layer.py
@@ -4,7 +4,17 @@ class Layer:
         self.output = None
 
     def forward(self, input):
+        '''
+        Forward propogation: Takes in a tensor input and 
+        returns some transformation as output
+        '''
         pass
 
     def backward(self, gradient, learning_rate):
+        '''
+        Backward propogation: Takes in the gradient of error 
+        with respect to output (gradient) then - (1) updates 
+        params (2) computes derivative of error with respect 
+        to input to pass to previous layer
+        '''
         pass
