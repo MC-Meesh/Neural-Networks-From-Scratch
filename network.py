50d8faa1a4b7f421bbee14a2904fd80421d3de56
Michael Allen <chaseallenskis@gmail.com>
2023-10-22 09:20:31 -0600

    Updated network layer to match col input

--- network.py
+++ network.py
@@ -4,17 +4,14 @@ def predict(network, input):
         output = layer.forward(output)
     return output
 
-def train(network, loss, loss_prime, x_train, y_train, epochs = 1000, learning_rate = 0.01, verbose = True):
+def train(network, loss, loss_prime, x_train, y_train, epochs = 1000, learning_rate = 0.001, verbose = True):
     for e in range(epochs):
         error = 0
         for x, y in zip(x_train, y_train):
-            # forward
             output = predict(network, x)
 
-            # error
             error += loss(y, output)
 
-            # backward
             grad = loss_prime(y, output)
             for layer in reversed(network):
                 grad = layer.backward(grad, learning_rate)
